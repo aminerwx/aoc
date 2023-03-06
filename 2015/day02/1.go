27626a52cb66c2944c9baa5a4f079a921ad7acf9
RSX-0806 <roronoa.z2022@gmail.com>
2023-03-06 14:34:35 +0100

    added day2

--- 2015/day02/1.go
+++ 2015/day02/1.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "os"
     "strings"
+    "strconv"
 )
 
 func puzzle_input() []string {
@@ -15,7 +16,33 @@ func puzzle_input() []string {
     return lines
 }
 
+
+func paper(row ...string) int {
+    l, _ := strconv.Atoi(row[0])
+    w, _ := strconv.Atoi(row[1])
+    h, _ := strconv.Atoi(row[2])
+    wrapping_paper := 2 * l * w + 2 * w * h + 2 * l * h
+    slack := min(l * w, min(w * h, h * l))
+    return wrapping_paper + slack
+}
+
+func min(a int, b int) int {
+    var ret int = 0
+    if  a < b {
+        ret = a
+    } else {
+        ret = b 
+    }
+    return ret
+}
+
 func main() {
    input := puzzle_input()
-
+   total := 0
+   for _, line := range input[:len(input) - 1] {
+      row := strings.Split(line, "x")
+      present := paper(row...)
+      total += present
+   } 
+   fmt.Println(total)  
 }
