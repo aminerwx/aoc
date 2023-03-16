838b7bd25dcc830b2cedcc1e4bb0ae245a326251
RSX-0806 <roronoa.z2022@gmail.com>
2023-03-16 05:13:58 +0100

    added day08

---
+++ 2015/day08/1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+)
+
+func puzzleInput() []byte {
+	_, file, _, _ := runtime.Caller(0)
+	folder := filepath.Dir(filepath.Dir(file))
+	dat, err := os.ReadFile(filepath.Join(folder, "input"))
+	if err != nil {
+		panic(err)
+	}
+	return dat
+}
+
+func main() {
+	input := puzzleInput()
+	lines := bytes.Split(input, []byte("\n"))
+	strCode := 0
+	strMemory := 0
+	for _, s := range lines[:len(lines)-1] {
+		strCode += len(s)
+		v, _ := strconv.Unquote(string(s))
+		strMemory += len(v)
+	}
+	fmt.Println(strCode, "-", strMemory, "=", strCode-strMemory)
+}
